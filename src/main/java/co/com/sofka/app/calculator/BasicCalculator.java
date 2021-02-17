6fdfb3da37a6d468c15d7c22df9ded8f1ccad457
motta1616 <ingeniero.leonardomotta@gmail.com>
2021-02-17 11:10:29 -0500

    Se agregan los operadores Aritmético en la clase BasicCalculator

---
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -0,0 +1,29 @@
+package co.com.sofka.app.calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicCalculator {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
+
+    public Long rest(Long number1, Long number2) {
+        logger.info( "Summing {} - {}", number1, number2 );
+        return number1 - number2;
+    }
+
+    public Long mult(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 * number2;
+    }
+
+    public Long divi(Long number1, Long number2) {
+        logger.info( "Summing {} / {}", number1, number2 );
+        return number1 / number2;
+    }
+}
